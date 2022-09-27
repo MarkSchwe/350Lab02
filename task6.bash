2fbb399b71286dbba309e726d5cb51dbab6283b0
MarkSchwe <91101294+MarkSchwe@users.noreply.github.com>
2022-09-26 20:35:48 -0400

    Update task6.bash

--- task6.bash
+++ task6.bash
@@ -1,57 +1,59 @@
 #!/bin/sh
 
-echo Input an odd number greater than 3:
+echo Input an odd number greater than 3: 
 read num
-if [ `expr $num % 2` -eq 0 ]; then
-  echo "Invalid response"
-  exit 1
+if [ `expr $num % 2`  -eq 0 ]; then
+	echo "Invalid response"
+	exit 1
 elif [ $num -lt 4 ]; then
-  echo "Invalid response"
-  exit 1
+	echo "Invalid response"
+	exit 1
 else
-  echo "$num is a valid response"
+	echo "$num is a Valid response"
 fi
 
 for i in $( seq 1 $num );
 do
-space=` expr $num - $i`
+space=` expr $num - $i` 
 if [ `expr $i % 2` = 1 ]; then
-  for x in $( seq 0 `expr $space / 2` );
-    do
-      echo -n " "
-    done
-  for y in $( seq 1 $i );
-    do
-      echo -n "*"
-    done
-  for z in $( seq 0 `expr $space / 2` );
-    do
-      echo -n " "
-    done
-  echo -n "\n"
-fi
+	for x in $( seq 0 `expr $space / 2` );
+		do
+			echo -n " "
+		done
+	for y in $( seq 1 $i );
+		do
+			echo -n "*"
+		done
+	for z in $( seq 0 `expr $space / 2 ` );
+		do
+			echo -n " "
+		done
+	echo -n "\n"
+	fi
 done
 
 num2=$num
 for i in $( seq 0 $num);
 do
-  num2=`expr $num2 - 1`
-  space=`expr $num - $num2`
-  if [ `expr $i % 2` = 1 ]; then
-    for x in $( seq 0 `expr $space / 2` );
-      do
-        echo -n " "
-      done
-    for y in $( seq 1 $num2 );
-      do
-        echo -n "*"
-      done
-    for z in $( seq 0 `expr $space / 2` );
-      do
-        echo -n " "
-      done
-    echo -n "\n"
-  fi
+	num2=`expr $num2 - 1`
+	space=`expr $num - $num2`
+	if [ `expr $i % 2` = 1 ]; then
+		for x in $(seq 0 `expr $space / 2` );
+		do
+			echo -n " "
+		done
+		for y in $(seq 1 $num2);
+		do
+			echo -n "*"
+		done
+		for z in $(seq 0 `expr $space / 2 ` );
+		do 
+			echo -n " "
+		done
+		echo -n "\n"
+	fi
 done
-  
+
+
+
 exit 0
